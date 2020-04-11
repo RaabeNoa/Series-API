f727e8520419b5c6171ad6623bf300916badb7f4
Raabe Noa <raabe.noa@gmail.com>
2020-04-11 16:24:18 -0300

    Add Model Episodio

---
+++ app/Episodio.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Episodio extends Model
+{
+    public $timestamps = false; //atributos created_at e update_at
+    protected $fillable = ['temporada', 'numero', 'assistido','serie_id'];
+    protected $appends = ['links'];
+    public function serie()
+    {
+        $this->belongsTo(Serie::class);
+    }
+
+    public function getAssistidoAttribute($assistido): bool
+    {
+        return $assistido;
+    }
+
+    public function getLinksAttribute($links): array
+    {
+        return [
+            'self' => '/api/episodios/' . $this->id,
+            'serie' => '/api/series/' . $this->serie_id
+        ];
+    }
+}
+
